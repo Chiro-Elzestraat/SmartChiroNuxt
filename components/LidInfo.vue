749cf9a110071701ae4df8e84f160b9a47ddb01b
mixxamm <maximjanssensj5@gmail.com>
2020-01-28 23:17:44 +0100

    Zet alle contactgegevens in v-cards

--- components/LidInfo.vue
+++ components/LidInfo.vue
@@ -6,40 +6,45 @@
       <div>
         <h1 class="title">Ouders</h1>
         <div class="contact">
-          <div
+          <v-card
+            outlined
             v-for="(ouder, i) in lid.contact.ouders"
             :key="i"
             class="contactpersoon"
           >
-            <h1 class="headline">Ouder {{ i + 1 }}</h1>
-            {{ ouder.naam }}<br />
-            <a :href="'tel:' + ouder.gsm">{{ ouder.gsm }}</a
-            ><br />
-            <a :href="'mailto:' + ouder.mail">{{ ouder.mail }}</a>
-          </div>
+            <v-card-title class="headline">Ouder {{ i + 1 }}</v-card-title>
+            <v-card-subtitle>{{ ouder.naam }}</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <a :href="'tel:' + ouder.gsm">{{ ouder.gsm }}</a
+              ><br />
+              <a :href="'mailto:' + ouder.mail">{{ ouder.mail }}</a>
+            </v-card-text>
+          </v-card>
         </div>
         <v-divider />
         <h1 class="title">Extra contactpersonen</h1>
         <div class="contact">
-          <div
+          <v-card
+            outlined
             v-for="(extra, i) in lid.contact.extra"
             :key="i"
             class="contactpersoon"
           >
-            <h1 class="headline">{{ extra.relatie }}</h1>
-            {{ extra.naam }}<br />
-            <a :href="'tel:' + extra.gsm">{{ extra.gsm }}</a>
-          </div>
-        </div>
-        <v-divider />
-        <div class="contact">
-          <div class="contactpersoon">
-            <h1 class="headline">Huisarts</h1>
-            {{ lid.contact.huisarts.naam }}<br />
-            <a :href="'tel:' + lid.contact.huisarts.gsm">{{
-              lid.contact.huisarts.gsm
-            }}</a>
-          </div>
+            <v-card-title class="headline">{{ extra.relatie }}</v-card-title>
+            <v-card-subtitle>{{ extra.naam }}</v-card-subtitle>
+            <v-card-text class="text--primary"
+              ><a :href="'tel:' + extra.gsm">{{ extra.gsm }}</a></v-card-text
+            >
+          </v-card>
+          <v-card outlined class="contactpersoon">
+            <v-card-title class="headline">Huisarts</v-card-title>
+            <v-card-subtitle>{{ lid.contact.huisarts.naam }}</v-card-subtitle>
+            <v-card-text class="text--primary"
+              ><a :href="'tel:' + lid.contact.huisarts.gsm">{{
+                lid.contact.huisarts.gsm
+              }}</a></v-card-text
+            >
+          </v-card>
         </div>
       </div>
     </v-tab-item>
@@ -47,7 +52,6 @@
       <div class="medischeInfo">
         <v-card class="medischeKaart" outlined>
           <v-card-title
-            class="headline text-center"
             :class="{
               goed: lid.medischeInfo.medischeHandelingen,
               belangrijk: !lid.medischeInfo.medischeHandelingen
@@ -106,5 +110,9 @@ export default {
 }
 .medischeKaart {
   margin: 8px;
+}
+.contactpersoon {
+  margin: 16px auto;
+  min-width: 200px;
 }
 </style>
