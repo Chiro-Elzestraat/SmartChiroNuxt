4e5777e70e995dd33438ceed30a2e45577761595
mixxamm <maximjanssensj5@gmail.com>
2020-01-31 18:34:48 +0100

    Maak optiedivider component

---
+++ components/OptieDivider.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="background">
+    <v-icon color="primary" class="icon">{{ icon }}</v-icon>
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.background {
+  display: flex;
+  align-items: center;
+  background-color: rgba(100, 100, 100, 0.1);
+  height: 46px;
+  width: 100%;
+}
+.icon {
+  margin-left: 28px;
+}
+span {
+  margin-left: 8px;
+}
+</style>
