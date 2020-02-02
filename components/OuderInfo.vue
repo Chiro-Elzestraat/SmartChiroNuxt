e43ae53f7904b6d1b4a70b5fe538c47e26e7a1c1
mixxamm <maximjanssensj5@gmail.com>
2020-02-02 13:16:18 +0100

    Maak OuderInfo component

---
+++ components/OuderInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card class="ouderinfo">
+    <v-row>
+      <v-col
+        ><v-text-field v-model="ouder.naam" label="Naam"></v-text-field
+      ></v-col>
+      <v-col
+        ><v-text-field
+          v-model="ouder.email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field
+      ></v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    ouderProp: {
+      type: Object,
+      default: () => {
+        return { naam: '', email: '', gsm: '' }
+      }
+    }
+  },
+  computed: {
+    ouder: {
+      get() {
+        return this.ouderProp
+      },
+      set(ouder) {
+        this.$emit('ouder-updatet', ouder)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.ouderinfo {
+  padding: 16px;
+}
+</style>
