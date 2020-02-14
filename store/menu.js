c9c508f96747a27a258d2ad2237be74885cd721b
Wankerke <wander_denies@hotmail.be>
2020-02-14 15:28:13 +0100

    dashboard aan menu

--- store/menu.js
+++ store/menu.js
@@ -1,7 +1,7 @@
 const getDefaultState = () => {
   return {
     items: [
-      { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
+      { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/dashboard' },
       { title: 'Mijn account', icon: 'mdi-account', to: '/account' },
       { title: 'Instellingen', icon: 'mdi-settings', to: '/instellingen' }
     ]
