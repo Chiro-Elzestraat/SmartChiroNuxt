fb180b0f230c5a740f4e774c8af3f8ef21ed9e05
mixxamm <maximjanssensj5@gmail.com>
2020-02-19 10:04:54 +0100

    Voeg laadicoontje toe bij betaling

--- components/BetalingInfo.vue
+++ components/BetalingInfo.vue
@@ -1,19 +1,32 @@
 <template>
   <v-card outlined>
     <v-card-text>
-      <v-expansion-panels>
-        <v-expansion-panel v-for="(betaling, index) in betalingen" :key="index">
-          <v-expansion-panel-header
-            >{{ betaling.jaar }} -
-            {{ betaling.jaar - 0 + 1 }}</v-expansion-panel-header
+      <div v-if="laden">
+        <v-progress-circular
+          indeterminate
+          style="margin: 0 auto;display: block;"
+        ></v-progress-circular>
+      </div>
+      <div v-else>
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(betaling, index) in betalingen"
+            :key="index"
           >
-          <v-expansion-panel-content>
-            <p>Betalingsnummer: {{ betaling.betalingsnummer }}</p>
-            <p>Bedrag: {{ betaling.bedrag }}</p>
-            <v-icon>{{ betaling.betaald ? `mdi-check` : `mdi-close` }}</v-icon>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+            <v-expansion-panel-header
+              >{{ betaling.jaar }} -
+              {{ betaling.jaar - 0 + 1 }}</v-expansion-panel-header
+            >
+            <v-expansion-panel-content>
+              <p>Betalingsnummer: {{ betaling.betalingsnummer }}</p>
+              <p>Bedrag: {{ betaling.bedrag }}</p>
+              <v-icon>{{
+                betaling.betaald ? `mdi-check` : `mdi-close`
+              }}</v-icon>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
     </v-card-text>
   </v-card>
 </template>
@@ -23,7 +36,8 @@ import { db } from '@/plugins/firebase'
 export default {
   data() {
     return {
-      betalingen: []
+      betalingen: [],
+      laden: true
     }
   },
   props: {
@@ -37,7 +51,10 @@ export default {
       .doc(this.lidId)
       .collection('betaling')
       .get()
-      .then((snap) => snap.forEach((doc) => this.betalingen.push(doc.data())))
+      .then((snap) => {
+        snap.forEach((doc) => this.betalingen.push(doc.data()))
+        this.laden = false
+      })
   }
 }
 </script>
