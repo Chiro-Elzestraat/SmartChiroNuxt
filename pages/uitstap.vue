b2bcd5377fa6e954edbfc8836c34cfa5072fe628
mixxamm <maximjanssensj5@gmail.com>
2020-02-22 17:40:07 +0100

    Toon uitstappen
    Moet nog uitgebreid worden.

--- pages/uitstap.vue
+++ pages/uitstap.vue
@@ -1,9 +1,48 @@
 <template>
   <div>
     <h1>Uitstappen</h1>
-    <v-card>
-      <v-card-title>Test</v-card-title>
-      <v-card-subtitle>Test</v-card-subtitle>
+    <!-- <v-card>
+      <v-card-title></v-card-title>
+      <v-card-subtitle></v-card-subtitle>
+      <v-img :src="uitstap.url"></v-img>
+    </v-card> -->
+    <v-card
+      class="card"
+      max-width="400"
+      v-for="(uitstap, index) in uitstappen"
+      :key="index"
+    >
+      <v-img class="white--text align-end" height="200px" :src="uitstap.url">
+        <v-card-title>{{ uitstap.titel }}</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pb-0"
+        >{{ uitstap.dates[0] }} - {{ uitstap.dates[1] }}</v-card-subtitle
+      >
+
+      <v-card-text class="text--primary">
+        {{ uitstap.beschrijving }}
+        <v-row>
+          <v-chip
+            class="groep"
+            :color="groep.geselecteerd ? 'green' : ''"
+            @click="groep.geselecteerd = !groep.geselecteerd"
+            v-for="(groep, index) in uitstap.groepen"
+            :key="index"
+            >{{ groep.naam }}</v-chip
+          >
+        </v-row>
+      </v-card-text>
+
+      <!-- <v-card-actions>
+        <v-btn color="orange" text>
+          Share
+        </v-btn>
+
+        <v-btn color="orange" text>
+          Explore
+        </v-btn>
+      </v-card-actions> -->
     </v-card>
     <v-dialog
       v-model="toevoegen"
@@ -27,6 +66,7 @@
 
 <script>
 import firebase from 'firebase'
+import { db, storage } from '@/plugins/firebase'
 import NieuweUitstap from '@/components/NieuweUitstap'
 export default {
   components: {
@@ -35,7 +75,8 @@ export default {
   data() {
     return {
       toevoegen: false,
-      leider: false
+      leider: false,
+      uitstappen: []
     }
   },
   created() {
@@ -51,6 +92,24 @@ export default {
           console.log(error)
         })
     })
+  },
+  mounted() {
+    db.collection('uitstap')
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          storage
+            .ref(`uitstap/${doc.id}`)
+            .getDownloadURL()
+            .then((url) => {
+              console.log(url)
+              this.uitstappen.push({ ...doc.data(), url })
+            })
+            .catch(function(error) {
+              console.log(error)
+            })
+        })
+      })
   }
 }
 </script>
@@ -64,5 +123,8 @@ export default {
 }
 .groep {
   margin: 8px;
+}
+.card {
+  margin: 16px;
 }
 </style>
