58a9d6ca1f9e89882aa611a289337e615399a58b
mixxamm <maximjanssensj5@gmail.com>
2020-02-22 12:23:06 +0100

    Fix #48

--- components/DashboardLeider.vue
+++ components/DashboardLeider.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
     <v-row dense>
-      <v-col v-for="card in cards" :key="card.title">
-        <v-card class="mx-auto" @click="$router.push(card.page)" width="400px">
-          <v-img :src="card.src" height="200px" width="400px" contain></v-img>
-          <v-card-title v-text="card.title"></v-card-title>
-          <v-card-actions class="justify-center">
-            <v-btn outlined color="primary" text>Ga naar</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
+      <template v-for="card in cards">
+        <v-col v-if="!card.rol || rollen.includes(card.rol)" :key="card.title">
+          <v-card
+            class="mx-auto"
+            @click="$router.push(card.page)"
+            width="400px"
+          >
+            <v-img :src="card.src" height="200px" width="400px" contain></v-img>
+            <v-card-title v-text="card.title"></v-card-title>
+            <v-card-actions class="justify-center">
+              <v-btn outlined color="primary" text>Ga naar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </template>
     </v-row>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   data() {
     return {
@@ -23,7 +30,8 @@ export default {
           title: 'Financiën',
           icon: 'mdi-dashboard',
           src: '/financien.svg',
-          page: '/finance'
+          page: '/finance',
+          rol: 'kas'
         },
         {
           title: 'Verhuur',
@@ -37,8 +45,24 @@ export default {
           src: '/evenementen.svg',
           page: '/events'
         }
-      ]
+      ],
+      rollen: []
     }
+  },
+  created() {
+    firebase
+      .auth()
+      .currentUser.getIdTokenResult()
+      .then((idTokenResult) => {
+        if (idTokenResult.claims.kas) {
+          // Show admin UI.
+          this.rollen.push('kas')
+        } else {
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 }
 </script>
