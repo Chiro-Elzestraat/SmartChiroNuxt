8f4cceb985c133d514161b8785561852ee2539b8
Wankerke <wander_denies@hotmail.be>
2020-03-17 00:28:20 +0100

    geen idee wat dit nog is

--- components/Agenda.vue
+++ components/Agenda.vue
@@ -21,13 +21,15 @@
             ref="calendar"
             v-model="focus"
             color="primary"
+            :start="begindatum"
+            :end="einddatum"
             :events="events"
             :event-color="getEventColor"
             :now="today"
             :type="type"
             locale='nl'
             :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-            
+            @change="getEvents"
           ></v-calendar>
           <v-menu
             v-model="selectedOpen"
@@ -51,120 +53,157 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      today: new Date().toISOString().split('T')[0],
-      focus: new Date().toISOString().split('T')[0],
-      type: 'month',
-      start: null,
-      end: null,
-      selectedEvent: {},
-      selectedElement: null,
-      selectedOpen: false,
-      events: [],
-      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-      names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-    }),
-    computed: {
-      title () {
-        const { start, end } = this
-        if (!start || !end) {
-          return ''
-        }
-        const startMonth = this.monthFormatter(start)
-        const endMonth = this.monthFormatter(end)
-        const suffixMonth = startMonth === endMonth ? '' : endMonth
-        const startYear = start.year
-        const endYear = end.year
-        const suffixYear = startYear === endYear ? '' : endYear
-        const startDay = start.day + this.nth(start.day)
-        const endDay = end.day + this.nth(end.day)
-        switch (this.type) {
-          case 'month':
-            return `${startMonth} ${startYear}`
-        }
+
+import { db } from '@/plugins/firebase'
+
+export default {
+  data: () => ({
+    today: new Date().toISOString().split('T')[0],
+    focus: new Date().toISOString().split('T')[0],
+    type: 'month',
+    start: null,
+    end: null,
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+    //colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+    //names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+  }),
+  computed: {
+    title () {
+      const { start, end } = this
+      if (!start || !end) {
         return ''
-      },
-      monthFormatter () {
-        return this.$refs.calendar.getFormatter({
-          timeZone: 'UTC', month: 'long',
-        })
-      },
+      }
+      const startMonth = this.monthFormatter(start)
+      const endMonth = this.monthFormatter(end)
+      const suffixMonth = startMonth === endMonth ? '' : endMonth
+      const startYear = start.year
+      const endYear = end.year
+      const suffixYear = startYear === endYear ? '' : endYear
+      const startDay = start.day + this.nth(start.day)
+      const endDay = end.day + this.nth(end.day)
+      switch (this.type) {
+        case 'month':
+          return `${startMonth} ${startYear}`
+      }
+      return ''
     },
-    mounted () {
-      this.$refs.calendar.checkChange()
+    monthFormatter () {
+      return this.$refs.calendar.getFormatter({
+        timeZone: 'UTC', month: 'long',
+      })
     },
-    methods: {
-      viewDay ({ date }) {
-        this.focus = date
-        this.type = 'day'
-      },
-      getEventColor (event) {
-        return event.color
-      },
-      setToday () {
-        this.focus = this.today
-      },
-      prev () {
-        this.$refs.calendar.prev()
-      },
-      next () {
-        this.$refs.calendar.next()
-      },
-      showEvent ({ nativeEvent, event }) {
-        const open = () => {
-          this.selectedEvent = event
-          this.selectedElement = nativeEvent.target
-          setTimeout(() => this.selectedOpen = true, 10)
-        }
-        if (this.selectedOpen) {
-          this.selectedOpen = false
-          setTimeout(open, 10)
-        } else {
-          open()
-        }
-        nativeEvent.stopPropagation()
-      },
-      updateRange ({ start, end }) {
+  },
+  mounted () {
+    db.collection('verhuur')
+      .get()
+      .then((snapshot) => {
         const events = []
-        const min = new Date(`${start.date}T00:00:00`)
-        const max = new Date(`${end.date}T23:59:59`)
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
+        snapshot.forEach((events) => 
+          events.push({
+            begindatum: verhuur.dates[0],
+            einddatum: verhuur.dates[1]
+          })
+        )
+        this.events = events
+     })
+  },
+  methods: {
+    viewDay ({ date }) {
+      this.focus = date
+      this.type = 'day'
+    },
+    getEventColor (event) {
+      return event.color
+    },
+    /*getEvents ({ start, end }) {
+        const events = []
+        //const min = new Date(`${start.date}T00:00:00`)
+        //const max = new Date(`${end.date}T23:59:59`)
+        //const days = (max.getTime() - min.getTime()) / 86400000
+        //const eventCount = this.rnd(days, days + 20)
+        verhuur.forEach(element => {
+          //const allDay = this.rnd(0, 3) === 0
+          //const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          //const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          //const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          //const second = new Date(first.getTime() + secondTimestamp)
           events.push({
             name: this.names[this.rnd(0, this.names.length - 1)],
             start: this.formatDate(first, !allDay),
             end: this.formatDate(second, !allDay),
             color: this.colors[this.rnd(0, this.colors.length - 1)],
           })
-        }
-        this.start = start
-        this.end = end
+        });
         this.events = events
-      },
-      nth (d) {
-        return d > 3 && d < 21
-          ? 'th'
-          : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
-      },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
-      formatDate (a, withTime) {
-        return withTime
-          ? `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
-          : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`
-      },
+      },*/
+    setToday () {
+      this.focus = this.today
     },
-  }
+    prev () {
+      this.$refs.calendar.prev()
+    },
+    next () {
+      this.$refs.calendar.next()
+    },
+    showEvent ({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event
+        this.selectedElement = nativeEvent.target
+        setTimeout(() => this.selectedOpen = true, 10)
+      }
+      if (this.selectedOpen) {
+        this.selectedOpen = false
+        setTimeout(open, 10)
+      } else {
+        open()
+      }
+      nativeEvent.stopPropagation()
+    },
+    /*updateRange ({ start, end }) {
+      const events = []
+      const min = new Date(`${start.date}T00:00:00`)
+      const max = new Date(`${end.date}T23:59:59`)
+      const days = (max.getTime() - min.getTime()) / 86400000
+      const eventCount = this.rnd(days, days + 20)
+      for (let i = 0; i < eventCount; i++) {
+        const allDay = this.rnd(0, 3) === 0
+        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+        const second = new Date(first.getTime() + secondTimestamp)
+        events.push({
+          name: this.names[this.rnd(0, this.names.length - 1)],
+          start: this.formatDate(first, !allDay),
+          end: this.formatDate(second, !allDay),
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+        })
+      }
+      this.start = start
+      this.end = end
+      this.events = events
+    },*/
+    nth (d) {
+      return d > 3 && d < 21
+        ? 'th'
+        : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
+    },
+    rnd (a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a
+    },
+    formatDate (a, withTime) {
+      return withTime
+        ? `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
+        : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`
+    },
+  },
+}
 </script>
 
 <style>
 
 </style>
+
+<!--https://github.com/vuetifyjs/vuetify/blob/master/packages/docs/src/examples/calendars/playground.vue-->
