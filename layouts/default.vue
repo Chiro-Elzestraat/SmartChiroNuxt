c74729cde1e1c33d81c701c5bae07c4a4c73c88f
mixxamm <maximjanssensj5@gmail.com>
2020-04-16 12:48:04 +0200

    Update covid waarschuwing

--- layouts/default.vue
+++ layouts/default.vue
@@ -68,7 +68,7 @@
     <v-content>
       <v-container>
         <v-alert v-if="!this.$store.state.gebruiker.user.leider" type="error">
-          Wegens het coronavirus zijn de chirozondagen tot en met 19 april
+          Wegens het coronavirus zijn de chirozondagen tot en met 3 mei
           afgelast.
         </v-alert>
         <nuxt />
