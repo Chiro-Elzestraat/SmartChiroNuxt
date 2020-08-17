6ec60bc436c3c31fa720837442b24a7b123131ef
mixxamm <maximjanssensj5@gmail.com>
2020-08-17 14:58:00 +0200

    Maak toevoegen verhuurdatum mogelijk
    Gegevens van de huurder zullen worden opgeslagen in een subcollectie om diens gegevens te beschermen.

--- pages/verhuur.vue
+++ pages/verhuur.vue
@@ -8,13 +8,16 @@
           <v-card>
             <v-toolbar dark color="primary">
               <v-btn @click="nieuweBoekingDialog = false" icon dark><v-icon>mdi-close</v-icon></v-btn>
+              <v-spacer/>
+              <v-btn @click="voegVerhuurToe" text>Toevoegen</v-btn>
             </v-toolbar>
             <v-container>
              <v-row>
-               <v-col cols xs="12" sm="12" md="8"><v-text-field v-model="boeking.huurder.gsm" label="GSM"/>
-                 <v-text-field v-model="boeking.huurder.email" label="Email"/>
-                 <v-text-field v-model="boeking.huurder.naam" label="Naam"/>
+               <v-col cols xs="12" sm="12" md="8">
                  <v-text-field v-model="boeking.huurder.vereniging" label="Vereniging"/>
+                 <v-text-field v-model="boeking.huurder.naam" label="Naam"/>
+                 <v-text-field v-model="boeking.huurder.email" label="Email"/>
+                 <v-text-field v-model="boeking.huurder.gsm" label="GSM"/>
                  <v-row>
                    <v-col cols="auto">
                      <v-checkbox v-model="boeking.contractInorde" label="Contract in orde"></v-checkbox>
@@ -54,6 +57,7 @@
 </template>
 
 <script>
+  import { db } from '@/plugins/firebase'
 export default {
   data() {
     return {
@@ -69,6 +73,15 @@ export default {
         },
         opmerking: '',
       },
+    }
+  },
+  methods: {
+    voegVerhuurToe() {
+      db.collection('verhuur').add({datumAanvraag: this.boeking.datumAanvraag, dates: this.boeking.dates}).then(ref => {
+        ref.collection('huurder').doc('info').set({...this.boeking.huurder, opmerking: this.boeking.opmerking}).then(() => {
+          this.nieuweBoekingDialog = false
+        })
+      })
     }
   }
 }
