05062a4bad72e5a329391d15b48737f2921eb622
mixxamm <maximjanssensj5@gmail.com>
2020-11-08 17:28:13 +0100

    Haal betaalde leden op en toon in lijst
    Co-authored-by: Wankerke <wander_denies@hotmail.be>

--- components/OnzeChiro/Secretaris.vue
+++ components/OnzeChiro/Secretaris.vue
@@ -14,7 +14,7 @@
           >
             <v-icon>mdi-close</v-icon>
           </v-btn>
-          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-toolbar-title>Secretaris</v-toolbar-title>
           <v-spacer></v-spacer>
           <v-toolbar-items>
             <v-btn
@@ -26,6 +26,51 @@
             </v-btn>
           </v-toolbar-items>
             </v-toolbar>
+        <v-card flat>
+          <v-card-text>
+            <v-expansion-panels focusable popout>
+              <v-expansion-panel v-for="(gapLid, i) in gapLeden || 0" :key="i">
+                <v-expansion-panel-header>
+                  {{ gapLid.naam }}
+                  <v-spacer />
+                  {{
+                  new Date(gapLid.geboortedatum).toLocaleDateString('nl-NL', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                  })
+                  }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-card>
+                  <v-card
+            v-for="(gapOuder, i) in gapLid.contact.ouders"
+            :key="i"
+            outlined
+            class="contactpersoon"
+          >
+            <v-card-title class="headline">Ouder {{ i + 1 }}</v-card-title>
+            <v-card-subtitle>{{ gapOuder.naam }}</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <p>{{ gapOuder.gsm }}</p>
+              <p>{{ gapOuder.email }}</p>
+            </v-card-text>
+          </v-card>
+                    <div v-if="gapLid.adres">
+                      <v-card-title>
+                        <h5>adres</h5>
+                      </v-card-title>
+                      <v-card-text>
+                        {{ gapLid.adres.straat }} {{ gapLid.adres.huisnummer }}
+                        {{ gapLid.adres.bus }} {{ gapLid.adres.postcode }} {{ gapLid.adres.plaats }}
+                      </v-card-text>
+                    </div>
+                  </v-card>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card-text>
+        </v-card>
         </v-card>
     </v-dialog>
 </template>
@@ -35,17 +80,34 @@ export default {
     data() {
         return {
             dialog: false,
-            leden: []
+            gapLeden: [],
+            jaar: (new Date().getMonth() < 7 ? -1 : 0) + new Date().getFullYear()
         }
     },
     created(){
         // TODO: 2020 nog veranderen naar een berekende waarde
-        const betalingen = db.collectionGroup('betaling').where('jaar', '==', '2021')
+        const betalingen = db.collectionGroup('betaling').where('jaar', '==', this.jaar).where('betaald', '==', true)
         betalingen.get().then(snapshot => {
-            snapshot.forEach(doc => {
-                console.log(doc.ref.parent.get())
-            })
+         snapshot.forEach(doc => {
+           doc.ref.parent.parent.get().then(doc1 => this.gapLeden.push({...doc1.data()}))
+         })
         })
-    }
+    },
+    methods: {
+      name() {
+        
+      }
+    },
 }
 </script>
+
+<style>
+.contactpersoon {
+  margin: 16px auto;
+  min-width: 200px;
+}
+.contact {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
