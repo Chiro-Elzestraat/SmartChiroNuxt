a692ac8e6056da656f774abeceefd91b887d2e86
mixxamm <maximjanssensj5@gmail.com>
2022-06-18 20:35:23 +0200

    Voeg mogelijkheid tot kopieren mails per groep toe

--- pages/leden.vue
+++ pages/leden.vue
@@ -4,21 +4,15 @@
       <v-toolbar-title>Overzicht leden</v-toolbar-title>
       <div class="mt-8 ml-10">
         <v-col cols xs="8" sm="8" md="8">
-          <v-select
-            @change="vraagLedenOp"
-            v-model="geselecteerdJaar"
-            :items="selectJaren"
-            outlined
-          ></v-select>
+          <v-select @change="vraagLedenOp" v-model="geselecteerdJaar" :items="selectJaren" outlined></v-select>
         </v-col>
       </div>
       <v-spacer />
-      <v-btn @click="krijgMails"><v-icon>mdi-content-copy</v-icon> mails</v-btn>
+      <v-btn @click="mailsDialog = true">
+        <v-icon>mdi-content-copy</v-icon> mails
+      </v-btn>
     </v-toolbar>
-    <v-tabs
-      :vertical="!this.$device.isMobile"
-      :show-arrows="this.$device.isMobile"
-    >
+    <v-tabs :vertical="!this.$device.isMobile" :show-arrows="this.$device.isMobile">
       <v-tab v-for="(groep, i) in groepen" :key="i" class="vtab">
         <!-- <v-icon left>mdi-account</v-icon> -->
         {{ groep.naam }}
@@ -32,16 +26,16 @@
               <v-expansion-panel v-for="(lid, i) in groep.leden || 0" :key="i">
                 <v-expansion-panel-header :color="lid.betaald ? '' : 'red'">
                   <v-badge v-if="lid.waarschuwing" icon="mdi-alert" left>{{
-                    lid.naam
+                      lid.naam
                   }}</v-badge>
                   <div v-else>{{ lid.naam }}</div>
                   <v-spacer />
                   {{
-                    new Date(lid.geboortedatum).toLocaleDateString('nl-NL', {
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric'
-                    })
+                      new Date(lid.geboortedatum).toLocaleDateString('nl-NL', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                      })
                   }}
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
@@ -64,15 +58,15 @@
             <v-expansion-panels focusable popout>
               <v-expansion-panel v-for="(leider, ii) in leiders" :key="ii">
                 <v-expansion-panel-header>{{
-                  leider.naam
+                    leider.naam
                 }}</v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  <v-card
-                    ><v-card-text>
+                  <v-card>
+                    <v-card-text>
                       <a :href="'tel:' + leider.gsm">
                         {{ leider.gsm }}
-                      </a></v-card-text
-                    >
+                      </a>
+                    </v-card-text>
                     <div v-if="leider.adres">
                       <v-card-text>
                         {{ leider.adres.straat }} {{ leider.adres.huisnummer }}
@@ -88,9 +82,27 @@
         </v-card>
       </v-tab-item>
     </v-tabs>
-    <v-snackbar v-model="gekopieerd"
-      >Mails van alle ouders gekopierd naar het klembord.</v-snackbar
-    >
+    <v-dialog v-model="mailsDialog" max-width="400px">
+      <v-card>
+        <v-card-title>Selecteer groepen</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+            <v-checkbox v-model="alles" label="Selecteer alles"></v-checkbox>
+            <v-checkbox v-model="speelclub" label="Speelclub"></v-checkbox>
+            <v-checkbox v-model="rakkers" label="Rakkers"></v-checkbox>
+            <v-checkbox v-model="toppers" label="Toppers"></v-checkbox>
+            <v-checkbox v-model="kerels" label="Kerels"></v-checkbox>
+            <v-checkbox v-model="aspis" label="Aspi's"></v-checkbox>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="blue darken-1" text @click="krijgMails">
+            Kopieer mails
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar v-model="gekopieerd">Mails gekopierd naar het klembord.</v-snackbar>
   </v-container>
 </template>
 
@@ -112,6 +124,13 @@ export default {
         { naam: `Aspi's`, leden: [], minLeeftijd: 16, maxLeeftijd: 18 }
       ],
       mails: [],
+      mailsDialog: false,
+      speelclub: false,
+      rakkers: false,
+      toppers: false,
+      kerels: false,
+      aspis: false,
+      alles: false,
       leiders: [],
       gekopieerd: false,
       chiroJaar:
@@ -122,6 +141,23 @@ export default {
   computed: {
     selectJaren() {
       return [this.chiroJaar, this.chiroJaar - 1, this.chiroJaar - 2]
+    }
+  },
+  watch: {
+    alles(nieuwAlles, oudAlles){
+      if(nieuwAlles){
+        this.speelclub = true
+        this.rakkers = true
+        this.toppers = true
+        this.kerels = true
+        this.aspis = true
+      } else {
+        this.speelclub = false
+        this.rakkers = false
+        this.toppers = false
+        this.kerels = false
+        this.aspis = false
+      }
     }
   },
   mounted() {
@@ -137,6 +173,43 @@ export default {
   },
   methods: {
     krijgMails() {
+      this.mails = []
+      if(this.speelclub){
+        this.groepen.find(groep => groep.naam === 'Speelclub').leden.forEach(lid => {
+          lid.contact.ouders.forEach(ouder => {
+            this.mails.push(ouder.email)
+          })
+        })
+      }
+      if(this.rakkers){
+        this.groepen.find(groep => groep.naam === 'Rakkers').leden.forEach(lid => {
+          lid.contact.ouders.forEach(ouder => {
+            this.mails.push(ouder.email)
+          })
+        })
+      }
+      if(this.toppers){
+        this.groepen.find(groep => groep.naam === 'Toppers').leden.forEach(lid => {
+          lid.contact.ouders.forEach(ouder => {
+            this.mails.push(ouder.email)
+          })
+        })
+      }
+      if(this.kerels){
+        this.groepen.find(groep => groep.naam === 'Kerels').leden.forEach(lid => {
+          lid.contact.ouders.forEach(ouder => {
+            this.mails.push(ouder.email)
+          })
+        })
+      }
+      if(this.aspis){
+        this.groepen.find(groep => groep.naam === 'Aspi\'s').leden.forEach(lid => {
+          lid.contact.ouders.forEach(ouder => {
+            this.mails.push(ouder.email)
+          })
+        })
+      }
+      this.mails = [...new Set(this.mails)]
       navigator.clipboard.writeText(this.mails.join(';')).then(() => {
         this.gekopieerd = true
       })
